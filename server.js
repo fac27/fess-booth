6d1b665d1fc7080ecfecb9733be0e06209786f89
cameochoquer <cjchoquer@gmail.com>
2023-05-16 17:36:36 +0100

    expanded sanitization functions & test

--- server.js
+++ server.js
@@ -6,7 +6,31 @@ server.get('/', (req, res) => {
 });
 
 const sanitize = (inputString) => {
-  return inputString.replace(/</g, "&lt;");
+  let sanitizedString = inputString;
+  
+  const characterMap = {
+    '<': '&lt;',
+    '>': '&gt;',
+    '&': '&amp;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    "/": '&#x2F;',
+    "`": '&#x60;',
+    "=": '&#x3D;',
+    "!": '&#x21;',
+    "(": '&#x28;',
+    ")": '&#x29;',
+    ";": '&#x3B;',
+    "+": '&#x2B;',
+    "-": '&#x2D;'
+  };
+
+  for (let character in characterMap) {
+    const replacement = characterMap[character];
+    sanitizedString = sanitizedString.replace(new RegExp("\\"+ character, 'g'), replacement);
+  }
+
+  return sanitizedString;
 };
 
 const validate = (message) => {
@@ -17,5 +41,5 @@ const validate = (message) => {
   }
 };
 
-
 module.exports = server;
+module.exports = { sanitize };
--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,26 @@
 const test = require("node:test");
 const assert = require("node:assert");
 const { request } = require("./helpers.js");
+const { sanitize } = require('../server.js');
+
+
+//testing creating newe post 
+test('should create a new post', async()=>{
+    const postData = {
+        title: 'Test post',
+        body: 'This is a test post.'
+      };
+    
+      const { status, body } = await request('/createPost', {
+        method: 'POST',
+        body: JSON.stringify(postData),
+        headers: { 'Content-Type': 'application/json' }
+      });
+    
+      assert.strictEqual(status, 200);
+      assert_attr(body, 'title', [postData.title], `Expected title to be '${postData.title}', but received:\n${body}`);
+      assert_attr(body, 'body', [postData.body], `Expected body to be '${postData.body}', but received:\n${body}`);
+})
 
 //testing sanitization function 
 test("POST with script tag sanitized", async()=>{
@@ -14,5 +34,17 @@ test("POST with script tag sanitized", async()=>{
         body,
         /&lt;script>alert\('uh oh'\)&lt;\/script>/i,
         `Expected <script> to have '<' replaced with '&lt;', but received:\n${body}`
+    );
+});
+
+//testing more complex sanitize function
+
+test("Sanitize function", ()=>{
+    const testString = "<>&\"'/`=!();+-";
+    const sanitizedString = sanitize(testString);
+    assert.equal(
+        sanitizedString,
+        "&amp&#x3B;lt&#x3B;&amp&#x3B;gt&#x3B;&amp&#x3B;&quot&#x3B;&#x27&#x3B;&#x2F&#x3B;&#x60&#x3B;&#x3D&#x3B;&#x21&#x3B;&#x28&#x3B;&#x29&#x3B;&#x3B;&#x2B;&#x2D;",
+        `Expected sanitized string, but received:\n${sanitizedString}`
     );
 });
