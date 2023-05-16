6e5034b740a217ae823f66cdb5502c7729c4ca64
cameochoquer <cjchoquer@gmail.com>
2023-05-16 12:15:50 +0100

    added test command in package.json file

--- server.js
+++ server.js
@@ -5,4 +5,17 @@ server.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+const sanitize = (inputString) => {
+  return inputString.replace(/</g, "&lt;");
+}
+
+const validate = (message) => {
+  if (message) {
+    return `<span style="color: red">${message}</span>`;
+  } else {
+    return "";
+  }
+}
+
+
 module.exports = server;
