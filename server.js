187c8f240f491578c115149fcc914e17cc7cd8f3
FomasTreeman <tom@team-freeman.com>
2023-05-16 15:35:04 +0100

    semi-colons

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ server.post("/", (req, res) => {
 
 const sanitize = (inputString) => {
   return inputString.replace(/</g, "&lt;");
-}
+};
 
 const validate = (message) => {
   if (message) {
@@ -101,7 +101,6 @@ const validate = (message) => {
   } else {
     return "";
   }
-}
-
+};
 
 module.exports = server;
