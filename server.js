f0f7e381f39f69f85f37a134ae6be8c90bc6741d
cameochoquer <cjchoquer@gmail.com>
2023-05-17 11:58:18 +0100

    updated req to res serve.js line 25

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ server.post("/", bodyParser, (req, res) => {
   }
   if(Object.keys(errors).length){
     const body = home(messages,errors,req.body);
-    req.statusCode(400).send(body);
+    res.statusCode(400).send(body);
   }else{
     const created = Date.now();
     messages.push({ name, message, created });
