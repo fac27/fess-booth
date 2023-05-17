17d52b48e80095a73f25acecc5a7d8a7df4dc355
cameochoquer <cjchoquer@gmail.com>
2023-05-17 12:55:32 +0100

    adding 'get' test

--- test/server.test.js
+++ test/server.test.js
@@ -16,3 +16,17 @@ test("POST with script tag sanitized", async()=>{
         `Expected <script> to have '<' replaced with '&lt;', but received:\n${body}`
     );
 });
+
+test("POST request with form data", async () => {
+    const app = server.listen(9876);
+
+    const { status, body } = await request("/", {
+      method: "POST",
+      body: "name=cc&message=hello",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+    });
+  
+    assert.equal(status, 200);
+    assert.match(body, /cc/i);
+    assert.match(body, /hello/i);
+  });
