172c326bcf97529e909603af4d2044ff21b31bf9
FomasTreeman <tom@team-freeman.com>
2023-05-17 15:34:33 +0100

    delete funcional

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require("express");
+const express = require('express');
 const server = express();
-const getEmoji = require("get-random-emoji");
-const { home } = require("./utils/template.js");
+const getEmoji = require('get-random-emoji');
+const { home } = require('./utils/template.js');
 
-server.use(express.static("public"));
+server.use(express.static('public'));
 const bodyParser = express.urlencoded({ extended: true });
 
 const messages = [];
 
-server.get("/", (req, res) => {
+server.get('/', (req, res) => {
   const body = home(messages);
   res.send(body);
 });
 
-server.post("/", bodyParser, (req, res) => {
+server.post('/', bodyParser, (req, res) => {
   const { name, message } = req.body;
 
   const errors = {};
   if (!name) {
-    errors.name = "please enter your name";
+    errors.name = 'please enter your name';
   }
   if (!message) {
-    errors.message = "please enter a message";
+    errors.message = 'please enter a message';
   }
   if (Object.keys(errors).length) {
     const body = home(messages, errors, req.body);
@@ -30,8 +30,16 @@ server.post("/", bodyParser, (req, res) => {
     emoji = getEmoji();
     const created = Date.now();
     messages.push({ name, emoji, message, created });
-    res.redirect("/");
+    res.redirect('/');
   }
+});
+
+server.post('/delete/:emoji', (req, res) => {
+  const { emoji } = req.params;
+  console.log(emoji);
+  const index = messages.findIndex((message) => message.emoji === emoji);
+  messages.splice(index, 1);
+  res.redirect('/');
 });
 
 module.exports = server;
