f477143e5a683384beb467b588157047f5aba854
Zakaria Yaris <info@zakariya.me>
2023-05-17 14:35:34 +0100

    unit test for checking user name and message

--- test/server.test.js
+++ test/server.test.js
@@ -3,32 +3,22 @@ const assert = require('node:assert');
 const { request } = require('../test/helpers.js');
 
 //testing sanitization function
-test('POST without nickname re-renders page with error', async () => {
+test('POST without name or message re-renders page with both errors', async () => {
   const { status, body } = await request('/', {
     method: 'POST',
-    body: 'nickname=&message=hello',
+    body: 'nickname=&message=',
     headers: { 'content-type': 'application/x-www-form-urlencoded' },
   });
   assert.equal(status, 400);
   assert.match(body, /<form/i, 'Page should include the form');
   assert.match(
     body,
-    /please enter your nickname/i,
+    /please enter your name/i,
     `Expected HTML to include "please enter your nickname", but received:\n${body}`
   );
+  assert.match(
+    body,
+    /please enter a message/i,
+    `Expected HTML to include "please enter a message", but received:\n${body}`
+  );
 });
-
-// test('POST request with form data', async () => {
-//   const app = server.listen(9876);
-
-//   const { status, body } = await request('/', {
-//     method: 'POST',
-//     body: 'name=cc&message=hello',
-//     headers: { 'content-type': 'application/x-www-form-urlencoded' },
-//   });
-//   app.close();
-
-//   assert.equal(status, 200);
-//   assert.match(body, /cc/i);
-//   assert.match(body, /hello/i);
-// });
