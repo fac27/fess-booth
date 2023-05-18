ac0af648f3c21a5fa9dbab91b7d48659eba673dd
FomasTreeman <tom@team-freeman.com>
2023-05-18 11:45:32 +0100

    src folder

--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,4 @@
-const server = require('../server');
+const server = require('../src/server');
 
 async function request(pathname, options = {}) {
   const app = server.listen(0);
--- utils/messageStore.js
+++ utils/messageStore.js
@@ -1,19 +1,19 @@
-const {readFile, writeFile, appendFile} = require("fs");
+const { readFile, writeFile, appendFile } = require('fs');
 
-const filePath = "./data/messages.json";
+const filePath = '../messages.json';
 
 const saveMessages = (messages) => {
-    return new Promise((resolve, reject) => {
-        writeFile(filePath, JSON.stringify(messages), (err) => {
-        if(err){
-            console.log("Error saving messages: " + err);
-            reject(err);
-        }else{
-            console.log("messages.json saved successfully");
-            resolve();
-        }
-        });
+  return new Promise((resolve, reject) => {
+    writeFile(filePath, JSON.stringify(messages), (err) => {
+      if (err) {
+        console.log('Error saving messages: ' + err);
+        reject(err);
+      } else {
+        console.log('messages.json saved successfully');
+        resolve();
+      }
     });
+  });
 };
 
-module.exports = {saveMessages};
+module.exports = { saveMessages };
