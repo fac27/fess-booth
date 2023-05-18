23fef6e0c1af4aa9c6f97a75558dade1348057cb
cameochoquer <cjchoquer@gmail.com>
2023-05-18 11:59:36 +0100

    reverted sanitise function

--- utils/sanitize.js
+++ utils/sanitize.js
@@ -1,23 +1,25 @@
-const characterMap = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#x27;',
-  '/': '&#x2F;',
-  '`': '&#x60;',
-  '=': '&#x3D;',
-  '!': '&#x21;',
-  '(': '&#x28;',
-  ')': '&#x29;',
-  '+': '&#x2B;',
-  '-': '&#x2D;'
-};
-
 const sanitize = (inputString) => {
-  let sanitizedString = inputString;
-  for (const char in characterMap ) {
-      sanitizedString = sanitizedString.split(char).join(characterMap [char]);
+  const sanitizedString = inputString;
+  
+  const characterMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    "/": '&#x2F;',
+    "`": '&#x60;',
+    "=": '&#x3D;',
+    "!": '&#x21;',
+    "(": '&#x28;',
+    ")": '&#x29;',
+    "+": '&#x2B;',
+    "-": '&#x2D;'
+  };
+
+  for (const character in characterMap) {
+    const replacement = characterMap[character];
+    sanitizedString = sanitizedString.replace(new RegExp("\\"+ character, 'g'), replacement);
   }
   return sanitizedString;
 };
